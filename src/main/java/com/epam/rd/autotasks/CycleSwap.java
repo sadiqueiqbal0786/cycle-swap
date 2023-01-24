e690eb6717cffd8253c21ca52459aa8c6e4afa19
Sadique Iqbal <sadiqueiqbal.si@gmail.com>
2023-01-24 12:34:07 +0530

    Finally

--- src/main/java/com/epam/rd/autotasks/CycleSwap.java
+++ src/main/java/com/epam/rd/autotasks/CycleSwap.java
@@ -2,20 +2,28 @@ package com.epam.rd.autotasks;
 
 class CycleSwap {
     static void cycleSwap(int[] array) {
+        if(array.length==0){
+            return;
+        }
+        else{
        int x = array[array.length-1], i;
         for (i = array.length-1; i > 0; i--)
             array[i] = array[i-1];
-        array[0] = x;
+        array[0] = x;}
 
     }
 
     static void cycleSwap(int[] array, int shift) {
+        if(array.length==0 || shift==0){
+            return;
+        }
+        else{
         for(int j=0;j<shift;j++) {
             int x = array[array.length - 1], i;
             for (i = array.length - 1; i > 0; i--)
                 array[i] = array[i - 1];
             array[0] = x;
-        }
+        }}
         //System.out.print(shift);
     }
 }
